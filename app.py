3b182c8752a310066f7af9123df01bd8098064d3
onss7750-prog <onss7750@gmail.com>
2026-03-23 10:37:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "LINE BOT OK"
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    return "OK"
+
+if __name__ == "__main__":
+    app.run()
